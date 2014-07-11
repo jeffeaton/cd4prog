21bebaf764114a15b494ad79ca25324b9baaac99
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2014-07-12 00:15:06 +0100

    updated likelihood.R to calculate P(t) analytically

--- likelihood.R
+++ likelihood.R
@@ -14,39 +14,37 @@ pexpsum <- Vectorize(function(x, rates){
   return(1-sum(sapply(seq_along(rates), function(i) prod(rates[-i])/prod(rates[-i] - rates[i])*exp(-x*rates[i]))))
   }, "x")
 
-## calculates expm(t*Lam), but appears slower than expm(t*Lam)
+## calculates expm(t*Lam) analytically, about 3-4 times faster than expm(t*Lam) for NS=4
 progmat <- function(t, lambda, mu){
 
   NS <- length(mu)
   rate <- c(lambda, 0) + mu
-  psurv <- c(lambda, 0)/rate
+  exp.rt <- exp(-t*rate)
 
   pmat <- matrix(0, NS, NS)
-
   for(i in 1:NS)
     for(j in i:NS){
-      ii <- (i-1)+seq_len(j-i+1)
-      iip <- (i-1)+seq_len(j-i)
-      pmat[i,j] <- (pexpsum(t, rate[iip]) - pexpsum(t, rate[ii]))*prod(psurv[iip])
+      if(i==j)
+        pmat[i,j] <- exp.rt[i]
+      else {
+        rvec <- rate[i:j]
+        val <- 0
+        for(ii in 1:(j-i+1))
+          val <- val+exp.rt[ii+i-1]/prod(rvec[ii] - rvec[-ii])
+        pmat[i,j] <- prod(lambda[i:(j-1)])*abs(val)
+      }
     }
 
   return(pmat)
-}
-      
+}      
 
 ## probability for an individual only with observed survival information
-ll.surv.ind <- function(t, d, t0=0, pi0, lambda, mu, Lam = NULL){
+ll.surv.ind <- function(t, d, t0=0, pi0, lambda, mu){
   ## t: time of last observation
   ## d: 1 = death, 0 = censored
-  ## Lam: transition matrix
 
-  if(is.null(Lam))
-    Lam <- transmat(lambda, mu)
-
-  pi.t <- pi0 %*% expm(t*Lam) # probability in stage s at time t;; !! THIS IS SLOW
-  ## pi.t <- expAtv(t(Lam), pi0, t)$eAtv # probability in stage s at time t;; appears slower for NS = 4
-  
-  psurv.t0 <- ifelse(t0 > 0, sum(pi0 %*% expm(t0*Lam)), 1) # probability of surviving to t0
+  pi.t <- pi0 %*% progmat(t, lambda, mu) # probability in stage s at time t
+  psurv.t0 <- ifelse(t0 > 0, sum(pi0 %*% progmat(t0, lambda, mu)), 1) # probability of surviving to t0
 
   if(d)
     return(log(sum(pi.t * mu)) - log(psurv.t0))
@@ -55,18 +53,15 @@ ll.surv.ind <- function(t, d, t0=0, pi0, lambda, mu, Lam = NULL){
 }
 
 ## probability for an individual with perfectly observed CD4 transition times (and censoring or death)
-ll.cd4.ind <- function(ts, t, d, t0=0, pi0, lambda, mu, Lam = NULL){
+ll.cd4.ind <- function(ts, t, d, t0=0, pi0, lambda, mu){
   ## ts: time of observed CD4 transitions, ts[initial stage] = 0.
   ## t: time of last observation
   ## d: 1 = death, 0 = censored
 
   NS <- length(pi0)
   
-  if(is.null(Lam))
-    Lam <- transmat(lambda, mu)
-
   s.t0 <- min(which(!is.na(ts)))
-  pi.t0 <- if(t0 > 0) as.vector(pi0 %*% expm(t0*Lam)) else pi0
+  pi.t0 <- if(t0 > 0) as.vector(pi0 %*% progmat(t0, lambda, mu)) else pi0
   log.p <- log(pi.t0[s.t0])  # probability of s.t0 is initial stage
 
   if(s.t0 < NS){
@@ -100,15 +95,13 @@ ll.cd4.ind <- function(ts, t, d, t0=0, pi0, lambda, mu, Lam = NULL){
 }
 
 ll.cd4dat <- function(pi0, lambda, mu, cd4dat){
-  Lam <- transmat(lambda, mu)
   if(is.null(cd4dat$t0)) cd4dat$t0 <- rep(0, length(cd4dat$t))
-  sum(sapply(1:length(cd4dat$t), function(i) ll.cd4.ind(cd4dat$ts[i,], cd4dat$t[i], cd4dat$d[i], cd4dat$t0[i], pi0, lambda, mu, Lam)))
+  sum(sapply(1:length(cd4dat$t), function(i) ll.cd4.ind(cd4dat$ts[i,], cd4dat$t[i], cd4dat$d[i], cd4dat$t0[i], pi0, lambda, mu)))
 }
 
 ll.survdat <- function(pi0, lambda, mu, survdat){
-  Lam <- transmat(lambda, mu)
   if(is.null(survdat$t0)) survdat$t0 <- rep(0, length(survdat$t))
-  return(sum(sapply(1:length(survdat$t), function(i) ll.surv.ind(survdat$t[i], survdat$d[i], survdat$t0[i], pi0, lambda, mu, Lam))))
+  return(sum(sapply(1:length(survdat$t), function(i) ll.surv.ind(survdat$t[i], survdat$d[i], survdat$t0[i], pi0, lambda, mu))))
 }
 
 ll <- function(theta, cd4dat, survdat=NULL, NS=4){
