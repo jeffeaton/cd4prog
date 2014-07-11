040275a7c2f76bf0b1a535b7738092084852997e
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2014-07-11 11:28:53 +0100

    updated likelihihood and CD4 transition imputation to account for left censoring

--- impute-transitions.R
+++ impute-transitions.R
@@ -1,6 +1,6 @@
 library(mvtnorm)
 
-sample.cd4trans <- function(pat.gam, cd4.thresh=c(500, 350, 200), rand=TRUE){
+impute.cd4trans <- function(pat.gam, cd4.thresh=c(500, 350, 200), rand=TRUE){
   ## pat.gam: GAM model output from mcgv package with spline output for individual patient CD4 trajectory
   ## rand: whether to sample random set of coefficients from covariance matrix (TRUE), or use coefficient point estimates (FALSE)
 
@@ -9,18 +9,20 @@ sample.cd4trans <- function(pat.gam, cd4.thresh=c(500, 350, 200), rand=TRUE){
   else
     coef.sim <- coef(pat.gam)
 
+  t0 <- min(pat.gam$smooth[[1]]$xp)
   t.final <- max(pat.gam$smooth[[1]]$xp)
   pred.sim <- function(x) sum(predict(pat.gam, list(cd4.t=x), type="lpmatrix") * coef.sim)
   
-  cd4.range <- c(pred.sim(0), pred.sim(t.final))  # !! THIS ASSUMES MONOTONIC DECLINE
+  cd4.range <- c(pred.sim(t0), pred.sim(t.final))  # !! THIS ASSUMES MONOTONIC DECLINE
 
   ts <- numeric(length(cd4.thresh)+1)
+  ts[1] <- t0
   for(i in 1:length(cd4.thresh))
-    ts[i+1] <- ifelse(cd4.range[1] < cd4.thresh[i], 0,          # started below
+    ts[i+1] <- ifelse(cd4.range[1] < cd4.thresh[i], t0,          # already below threshold at t0
                       ifelse(cd4.range[2] > cd4.thresh[i], NA,   # never reached CD4 threshold
-                             optimize(function(x) (pred.sim(x)-cd4.thresh[i])^2, lower=0, upper=t.final, tol=0.01)$minimum))
+                             optimize(function(x) (pred.sim(x)-cd4.thresh[i])^2, lower=t0, upper=t.final, tol=0.01)$minimum))
 
-  ts[1:length(ts) < max(which(ts==0))] <- NA
+  ts[1:length(ts) < max(which(ts==t0))] <- NA
 
   return(ts)
 }
--- likelihood.R
+++ likelihood.R
@@ -1,7 +1,5 @@
 library(expm)
 
-## NS <- 4 # number of stages
-
 transmat <- function(lambda, mu){
   Lam <- -diag(c(lambda, 0)+mu)
   diag(Lam[,-1]) <- lambda
@@ -37,7 +35,7 @@ progmat <- function(t, lambda, mu){
       
 
 ## probability for an individual only with observed survival information
-ll.surv.ind <- function(t, d, pi0, lambda, mu, Lam = NULL){
+ll.surv.ind <- function(t, d, t0=0, pi0, lambda, mu, Lam = NULL){
   ## t: time of last observation
   ## d: 1 = death, 0 = censored
   ## Lam: transition matrix
@@ -47,24 +45,32 @@ ll.surv.ind <- function(t, d, pi0, lambda, mu, Lam = NULL){
 
   pi.t <- pi0 %*% expm(t*Lam) # probability in stage s at time t;; !! THIS IS SLOW
   ## pi.t <- expAtv(t(Lam), pi0, t)$eAtv # probability in stage s at time t;; appears slower for NS = 4
+  
+  psurv.t0 <- ifelse(t0 > 0, sum(pi0 %*% expm(t0*Lam)), 1) # probability of surviving to t0
 
   if(d)
-    return(log(sum(pi.t * mu)))
+    return(log(sum(pi.t * mu)) - log(psurv.t0))
   else
     return(log(sum(pi.t)))
 }
 
 ## probability for an individual with perfectly observed CD4 transition times (and censoring or death)
-ll.cd4.ind <- function(ts, t, d, pi0, lambda, mu){
+ll.cd4.ind <- function(ts, t, d, t0=0, pi0, lambda, mu, Lam = NULL){
   ## ts: time of observed CD4 transitions, ts[initial stage] = 0.
   ## t: time of last observation
   ## d: 1 = death, 0 = censored
 
-  s0 <- which(ts==0)
-  log.p <- log(pi0[s0])  # probability that s is initial stage
+  NS <- length(pi0)
+  
+  if(is.null(Lam))
+    Lam <- transmat(lambda, mu)
 
-  if(s0 < NS){
-    for(s in s0:(NS-1)){  # this could probably be programmed more elegantly...
+  s.t0 <- min(which(!is.na(ts)))
+  pi.t0 <- if(t0 > 0) as.vector(pi0 %*% expm(t0*Lam)) else pi0
+  log.p <- log(pi.t0[s.t0])  # probability of s.t0 is initial stage
+
+  if(s.t0 < NS){
+    for(s in s.t0:(NS-1)){  # this could probably be programmed more elegantly...
       if(!is.na(ts[s+1])){
         dts <- ts[s+1] - ts[s]  # duration in stage s
         log.p <- log.p - dts*(lambda[s]+mu[s]) + log(lambda[s]) # P(trans after dts) = exp(-dts*(lambda[s]+mu[s])) * lambda[s]
@@ -94,15 +100,18 @@ ll.cd4.ind <- function(ts, t, d, pi0, lambda, mu){
 }
 
 ll.cd4dat <- function(pi0, lambda, mu, cd4dat){
-  sum(sapply(1:length(cd4dat$t), function(i) ll.cd4.ind(cd4dat$ts[i,], cd4dat$t[i], cd4dat$d[i], pi0, lambda, mu)))
+  Lam <- transmat(lambda, mu)
+  if(is.null(cd4dat$t0)) cd4dat$t0 <- rep(0, length(cd4dat$t))
+  sum(sapply(1:length(cd4dat$t), function(i) ll.cd4.ind(cd4dat$ts[i,], cd4dat$t[i], cd4dat$d[i], cd4dat$t0[i], pi0, lambda, mu, Lam)))
 }
 
 ll.survdat <- function(pi0, lambda, mu, survdat){
   Lam <- transmat(lambda, mu)
-  return(sum(sapply(1:length(survdat$t), function(i) ll.surv.ind(survdat$t[i], survdat$d[i], pi0, lambda, mu, Lam))))
+  if(is.null(survdat$t0)) survdat$t0 <- rep(0, length(survdat$t))
+  return(sum(sapply(1:length(survdat$t), function(i) ll.surv.ind(survdat$t[i], survdat$d[i], survdat$t0[i], pi0, lambda, mu, Lam))))
 }
 
-ll <- function(theta, cd4dat, survdat=NULL){
+ll <- function(theta, cd4dat, survdat=NULL, NS=4){
   pi0 <- c(1-sum(theta[1:(NS-1)]), theta[1:(NS-1)])  
   lambda <- exp(theta[(NS-1) + 1:(NS-1)])
   mu <- exp(theta[2*(NS-1) + 1:NS])
